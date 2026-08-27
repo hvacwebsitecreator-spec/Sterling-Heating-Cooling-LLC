9c6fb71763ebc67718ba0e1b92daf47a3ea889b1
hvacwebsitecreator-spec <hvacwebsitecreator@gmail.com>
2026-08-27 14:26:17 -0400

    Replace embedded logos with sterling-logo.png
    Replaced all instances of embedded logo images in index.html with sterling-logo.png, including updates to alt attributes.

---
+++ replace_all_logos.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = 'C:/Users/vedpa/OneDrive/Desktop/MONEY 2/index.html';
+let text = fs.readFileSync(path, 'utf8');
+text = text.replace(/<img[^>]*src="data:image\/[^\"]+"[^>]*>/gi, '<img src="sterling-logo.png" alt="Sterling Heating and Cooling logo" />');
+text = text.replace(/<img class="logo" src="sterling-logo\.png"[^>]*>/gi, '<img class="logo" src="sterling-logo.png" alt="Sterling Heating and Cooling logo" />');
+text = text.replace(/<img class="footer-logo" src="sterling-logo\.png"[^>]*>/gi, '<img class="footer-logo" src="sterling-logo.png" alt="Sterling Heating and Cooling logo" />');
+fs.writeFileSync(path, text, 'utf8');
+console.log('replaced all embedded logo images with sterling-logo.png');
